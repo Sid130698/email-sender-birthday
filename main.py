3e74d41012a046854c8ffba33c783fa7339fecff
Siddharth Singh <44903182+Sid130698@users.noreply.github.com>
2022-02-01 22:27:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import datetime
 
 
 #...........establishing connection............
-my_email= "thepythonbot1306@gmail.com"
+my_email= <yOUR_EMAIL>
 connection= smtplib.SMTP("smtp.gmail.com")
 
 #.......encrypting using tls to prevent data being leaked if intercepted...........
 
 connection.starttls()
-connection.login(user=my_email,password="sid130698")
+connection.login(user=my_email,password=<yOUR_PASSWORD>)
 
 #...........sending mail..............
 try:
     connection.sendmail(from_addr=my_email,
-    to_addrs="sidsingh130698@gmail.com",
+    to_addrs=<rECV_EMAIL>,
     msg="subject: Testing mail\n\nsending mail via bot")
 except:
     print("Error sending mail")
